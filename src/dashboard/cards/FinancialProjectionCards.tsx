03c40a2e3663a4c1a3f05c938c959f80f8937ff4
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-08 10:08:34 -0300

    feat: implementa useEffect para atualizar o valor de renda do mês de acordo com os dados do mock ou inseridos via formulário

--- src/dashboard/cards/FinancialProjectionCards.tsx
+++ src/dashboard/cards/FinancialProjectionCards.tsx
@@ -1,7 +1,32 @@
+import { useContext, useEffect, useState } from "react";
+import FinancialProfileContext from "@/context/FinancialProfileContext";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { TrendingDown, TrendingUp, Wallet } from "lucide-react";
+import { getCurrentMonthSalary, getTransactions } from "@/services/transactions.service";
 
 export default function FinancialProjectionCard() {
+  const { financialProfile } = useContext(FinancialProfileContext);
+  
+  const transactions = getTransactions();
+
+  const [currentIncome, setCurretIncome] = useState("0");
+
+  function formatValue(value: string | number) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(Number(value));
+  }
+
+  useEffect(() => {
+    const currentMonthIncome =
+      financialProfile.income === "0"
+        ? getCurrentMonthSalary(transactions)
+        : financialProfile.income;
+    const currentMonthIncomeFormatted = formatValue(currentMonthIncome);
+    setCurretIncome(currentMonthIncomeFormatted);
+  }, [financialProfile]);
+
   return (
     <Card className="min-w-[200px] max-w-[450px] min-h-[200px] max-h-[500px]">
       <CardHeader>
@@ -12,7 +37,7 @@ export default function FinancialProjectionCard() {
           <div className="flex flex-col items-center text-sm">
             <Wallet />
             <span>Renda do Mês</span>
-            <span>R$ 8500,00</span>
+            <span>{currentIncome}</span>
           </div>
           <div className="flex flex-col items-center text-sm">
             <TrendingDown />
@@ -20,7 +45,7 @@ export default function FinancialProjectionCard() {
             <span>R$ 6500,00</span>
           </div>
           <div className="flex flex-col items-center text-sm">
-           <TrendingUp />
+            <TrendingUp />
             <span>Saldo Atual</span>
             <span>R$ 2000,00</span>
           </div>
